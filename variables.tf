ec011e5f69eb623d21f034578c4f05f72789403d
KLee-2524 <klee.337x@gmail.com>
2025-07-21 00:51:33 -0700

    apt

--- variables.tf
+++ variables.tf
@@ -118,8 +118,8 @@ variable "github_target_setup_script" {
     apt update -y
     echo "apt update initiated" >> /home/ubuntu/FAST/setup_log.txt
 
-    export DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential 
-    export DEBIAN_FRONTEND=noninteractive apt-get install -y libpam0g-dev 
+    export DEBIAN_FRONTEND=noninteractive apt install -y build-essential 
+    export DEBIAN_FRONTEND=noninteractive apt install -y libpam0g-dev 
     echo "Build and dev tools installation initiated" >> /home/ubuntu/FAST/setup_log.txt
 
     mkdir /home/ubuntu/vsftpd234_lab
