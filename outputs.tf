1658a2d72ad4463c801b93491b401bd7254ce8ae
KLee-2524 <klee.337x@gmail.com>
2025-07-27 21:10:07 -0700

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,17 @@
-#placeholder
+output "kali_details" {
+    value = {
+        name       = aws_instance.kali-vm.tags["Name"]
+        public_ip  = aws_instance.kali-vm.public_ip
+        private_ip = aws_instance.kali-vm.private_ip
+    }
+    description = "Kali Linux EC2 instance details"
+}
+
+output "target_details" {
+    value = {
+        name       = aws_instance.vsftpd234-vm.tags["Name"]
+        public_ip  = aws_instance.vsftpd234-vm.public_ip
+        private_ip = aws_instance.vsftpd234-vm.private_ip
+    }
+    description = "Target EC2 instance details"
+}
