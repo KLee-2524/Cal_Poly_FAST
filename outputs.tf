a65c569c74ea3c985f9f1d8277584d3d65e08136
KLee-2524 <klee.337x@gmail.com>
2025-07-27 23:34:06 -0700

    modular output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "lab_instances" {
     value = {
-        kali_info   = module.vsftpd234-lab.output.kali_details
-        target_info = module.vsftpd234-lab.output.target_details
+        kali_info   = module.vsftpd234-lab.kali_details
+        target_info = module.vsftpd234-lab.target_details
     }
 }
