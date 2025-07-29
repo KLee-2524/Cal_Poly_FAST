0e7f2a81b99b013e716b76e5cb24261163550417
KLee-2524 <klee.337x@gmail.com>
2025-07-28 23:58:07 -0700

    outputs

--- modules/vsftpd234-lab/outputs.tf
+++ modules/vsftpd234-lab/outputs.tf
@@ -1,16 +1,40 @@
-output "kali_details" {
+output "kali_name" {
     value = {
         name       = aws_instance.kali-vm.tags["Name"]
-        public_ip  = aws_instance.kali-vm.public_ip
-        private_ip = aws_instance.kali-vm.private_ip
     }
-    description = "Kali Linux EC2 instance details"
+    description = "Kali Linux EC2 instance name"
 }
 
-output "target_details" {
+output "kali_pub_ip" {
+    value = {
+        public_ip  = aws_instance.kali-vm.public_ip
+    }
+    description = "Kali Linux EC2 instance public ip"
+}
+
+output "kali_pri_ip" {
+    value = {
+        private_ip = aws_instance.kali-vm.private_ip
+    }
+    description = "Kali Linux EC2 instance private ip"
+}
+
+output "target_name" {
     value = {
         name       = aws_instance.vsftpd234-vm.tags["Name"]
+    }
+    description = "Target EC2 instance details"
+}
+
+output "target_pub_ip" {
+    value = {
         public_ip  = aws_instance.vsftpd234-vm.public_ip
+    }
+    description = "Target EC2 instance details"
+}
+
+output "target_pri_ip" {
+    value = {
         private_ip = aws_instance.vsftpd234-vm.private_ip
     }
     description = "Target EC2 instance details"
--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,12 @@ output "lab_instances" {
     value = {
         for attendee, mod in module.vsftpd234-lab :
         attendee => {
-            kali_info   = module.vsftpd234-lab.kali_details
-            target_info = module.vsftpd234-lab.target_details
+            kali_name   = module.vsftpd234-lab.kali_name
+            kali_pub_ip = module.vsftpd234-lab.kali_pub_ip
+            kali_pri_ip = module.vsftpd234-lab.kali_pri_ip
+            target_name   = module.vsftpd234-lab.target_name
+            target_pub_ip = module.vsftpd234-lab.target_pub_ip
+            target_pri_ip = module.vsftpd234-lab.target_pri_ip
         }
     }
 }
