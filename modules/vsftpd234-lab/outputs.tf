1b1d40b4a2c85e0daed0a014ecb36f12ae05374c
KLee-2524 <klee.337x@gmail.com>
2025-07-29 23:56:42 -0700

    multi mod deployment

--- modules/vsftpd234-lab/outputs.tf
+++ modules/vsftpd234-lab/outputs.tf
@@ -1,41 +1,15 @@
-output "kali_name" {
+output "kali_details" {
     value = {
         name = aws_instance.kali-vm.tags["Name"]
+        pub_ip = aws_instance.kali-vm.public_ip
     }
     description = "Kali Linux EC2 instance name"
 }
 
-output "kali_pub_ip" {
-    value = {
-        public_ip = aws_instance.kali-vm.public_ip
-    }
-    description = "Kali Linux EC2 instance public ip"
-}
-
-output "kali_pri_ip" {
-    value = {
-        private_ip = aws_instance.kali-vm.private_ip
-    }
-    description = "Kali Linux EC2 instance private ip"
-}
-
-output "target_name" {
+output "target_details" {
     value = {
         name = aws_instance.vsftpd234-vm.tags["Name"]
-    }
-    description = "Target EC2 instance details"
-}
-
-output "target_pub_ip" {
-    value = {
-        public_ip = aws_instance.vsftpd234-vm.public_ip
-    }
-    description = "Target EC2 instance details"
-}
-
-output "target_pri_ip" {
-    value = {
-        private_ip = aws_instance.vsftpd234-vm.private_ip
+        pri_ip = aws_instance.vsftpd234-vm.private_ip
     }
     description = "Target EC2 instance details"
 }
