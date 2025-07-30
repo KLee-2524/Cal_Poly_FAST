dd7dd342f8d090deb22f5473b4f9538d7e52ddb9
KLee-2524 <klee.337x@gmail.com>
2025-07-29 23:59:07 -0700

    outputs

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "lab_instances" {
     value = {
         for attendee, mod in module.vsftpd234-lab :
         attendee => {
-            kali_name   = module.vsftpd234-lab["${attendee}].kali_name.name
-            kali_pub_ip = module.vsftpd234-lab["${attendee}].kali_pub_ip.kali_pub_ip
+            kali_name   = module.vsftpd234-lab["${attendee}].kali_details.name
+            kali_pub_ip = module.vsftpd234-lab["${attendee}].kali_details.pub_ip
             
-            target_name   = module.vsftpd234-lab["${attendee}].target_name.target_name
-            target_pri_ip = module.vsftpd234-lab["${attendee}].target_pri_ip.target_pri_ip
+            target_name   = module.vsftpd234-lab["${attendee}].target_details.name
+            target_pri_ip = module.vsftpd234-lab["${attendee}].target_details.pri_ip
         }
     }
 }
